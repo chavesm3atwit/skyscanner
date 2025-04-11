9a3230ed5f30b7013d0f78aca2f792a6611f4cd3
chavesm3atwit <98040062+chavesm3atwit@users.noreply.github.com>
2025-04-10 21:57:38 -0400

    search.java

---
+++ search.java
@@ -0,0 +1,25 @@
+package com.hoenscanner.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Search {
+
+    @JsonProperty
+    private String city;
+
+    public Search() {
+        // Default constructor for Jackson
+    }
+
+    public Search(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
